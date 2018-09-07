20240e1bb6ef46ecab50918c0383afdef6671e0c
Undigit Media <40588986+undigitmedia@users.noreply.github.com>
2018-09-07 18:35:46 +0530

    MyBtcFx-OTP SMS

---
+++ MyBtcFx/lambda.js
@@ -0,0 +1,32 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+	let receiver = event['receiver'];
+	let sender = event['sender'];
+	let message = event['message'];
+
+	console.log("Sending message", message, "to receiver", receiver);
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Transactional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			}
+		},
+		PhoneNumber: receiver
+	}).promise()
+		.then(data => {
+			console.log("OTP successfully delivered to", receiver);
+			callback(null, data);
+		})
+		.catch(err => {
+			console.log("Sending failed", err);
+			callback(err);
+		});
+}
